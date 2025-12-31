8a8dbc7b34c6fea56b08d9f93f72647bd5b7c3d7
clemberard <clement.berard@ecole-isitech.fr>
2025-12-31 10:15:52 +0100

    edit auteur

--- myproject/authors/urls.py
+++ myproject/authors/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('<int:author_id>/', views.show_author, name='show_author'),
     path('new/', views.new_view, name='new_author'),
     path('create/', views.create_view, name='create_author'),
+    path('edit/<int:author_id>/', views.edit_view, name='edit_author'),
+    path('update/<int:author_id>/', views.update_view, name='update_author')
 ]
--- myproject/authors/views.py
+++ myproject/authors/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from authors.models import Author
+from books.models import Book
 from .forms import AuthorForm
+import datetime
 
 # Create your views here.
 
@@ -9,15 +11,33 @@ def index(request):
     Affiche la liste de tous les auteurs.
     """
     authors = Author.objects.all()
+    total_authors = authors.count()
+    living_authors = authors.filter(death_date__isnull=True).count()
+    nationalities_count = authors.values('nationality').distinct().count()
+    total_books = Book.objects.count()
 
-    return render(request, 'authors/index.html', {'authors': authors})
+    return render(request, 'authors/index.html', {
+        'authors': authors,
+        'total_authors': total_authors,
+        'living_authors': living_authors,
+        'nationalities_count': nationalities_count,
+        'total_books': total_books
+    })
 
 def show_author(request, author_id):
     """
     Affiche les détails d'un auteur spécifique.
     """
     author = Author.objects.get(id=author_id)
-    return render(request, 'authors/show.html', {'author': author})
+    books = Book.objects.filter(author=author)
+    total_pages = sum(book.number_pages for book in books)
+    available_books = books.filter(copies_available__gt=0).count()
+    if author.death_date:
+        author_age = author.death_date.year - author.birth_date.year
+    else:
+        author_age = current_year - author.birth_date.year
+
+    return render(request, 'authors/show.html', {'author': author, 'books': books, 'total_pages': total_pages, 'available_books': available_books, 'author_age': author_age})
 
 def new_view(request):
     """
@@ -39,3 +59,27 @@ def create_view(request):
             return render(request, 'authors/new.html', {'form': form})
     else:
         form = AuthorForm()
+        
+def edit_view(request, author_id):
+    """
+    Affiche le formulaire pour éditer un auteur existant.
+    """
+    author = Author.objects.get(id=author_id)
+    form = AuthorForm(instance=author)
+    return render(request, 'authors/edit.html', {'form': form, 'author': author})
+
+def update_view(request, author_id):
+    """
+    Traite le formulaire de mise à jour d'un auteur existant.
+    """
+    author = Author.objects.get(id=author_id)
+    if request.method == 'POST':
+        form = AuthorForm(request.POST, request.FILES, instance=author)
+        if form.is_valid():
+            author = form.save()
+            return render(request, 'authors/show.html', {'author': author})
+        else:
+            return render(request, 'authors/edit.html', {'form': form, 'author': author})
+    else:
+        form = AuthorForm(instance=author)
+        return render(request, 'authors/edit.html', {'form': form, 'author': author})
